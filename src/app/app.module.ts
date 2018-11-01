a39a010d78880d63cb9af36c70c7ad74aac120d7
rkanumetta1 <44579556+rkanumetta1@users.noreply.github.com>
2018-11-01 15:20:59 -0700

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './route/app.route';
+import { HomecomponentComponent } from './components/homecomponent/homecomponent.component';
+import { HeadderComponent } from './components/headder/headder.component';
+import { CampaignmanagementComponent } from './components/campaignmanagement/campaignmanagement.component';
+import { LoginComponent } from './components/login/login.component';
+import { CreatecampaignComponent } from './components/createcampaign/createcampaign.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule, MatIconModule } from '@angular/material';
+import { InventorymanageComponent } from './components/inventortyManagement/inventorymanage/inventorymanage.component';
+import { ContentlibhomeComponent } from './components/ContentLibrary/contentlibhome/contentlibhome.component';
+import { CreatecontentComponent } from './components/ContentLibrary/createcontent/createcontent.component';
+import { HttpClientModule } from '@angular/common/http';
+import { EditcampaignComponent } from './components/editcampaign/editcampaign.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomecomponentComponent,
+    HeadderComponent,
+    CampaignmanagementComponent,
+    LoginComponent,
+    CreatecampaignComponent,
+    InventorymanageComponent,
+    ContentlibhomeComponent,
+    CreatecontentComponent,
+    EditcampaignComponent
+  ],
+  imports: [
+    BrowserModule,   
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatPaginatorModule ,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    HttpClientModule
+  ], 
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/route/app.route.ts
@@ -0,0 +1,74 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CampaignmanagementComponent } from '../components/campaignmanagement/campaignmanagement.component';
+import { LoginComponent } from '../components/login/login.component';
+import { HomecomponentComponent } from '../components/homecomponent/homecomponent.component';
+import { CreatecampaignComponent } from '../components/createcampaign/createcampaign.component';
+import { InventorymanageComponent } from '../components/inventortyManagement/inventorymanage/inventorymanage.component';
+import { ContentlibhomeComponent } from '../components/ContentLibrary/contentlibhome/contentlibhome.component';
+import { CreatecontentComponent } from '../components/ContentLibrary/createcontent/createcontent.component';
+import { EditcampaignComponent } from '../components/editcampaign/editcampaign.component';
+
+
+const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'/login',
+        pathMatch:'full'
+    },
+    { path: 'login', component: LoginComponent },
+    // {
+    //     path:'forgot',
+    //     component: ForgotComponent
+    // },
+    {
+        path:'home',
+        component: HomecomponentComponent,
+        children:[
+            {
+                path:'campaignmanagement',
+                component: CampaignmanagementComponent
+            },  
+            {
+                path:'createcampaign',
+                component: CreatecampaignComponent
+            },
+            {
+                path:'editcamp',
+                component:EditcampaignComponent
+            }
+        ]
+
+        
+    },
+
+    {
+        path:'invnt',
+        component:InventorymanageComponent
+    },
+    {
+        path:'contlib',
+        component:ContentlibhomeComponent,
+        children:[{
+           path:'',
+           redirectTo:'contentlibhome',
+           pathMatch:"full",
+           
+      },{
+          path:'contentlibhome',
+         component:   ContentlibhomeComponent
+      },{
+        path:'createcontent',
+       component:CreatecontentComponent
+      }
+]
+    }
+    
+    
+  ];
+  
+  @NgModule({
+      imports:[RouterModule.forRoot(routes)],
+      exports : [RouterModule]
+  })
+  export class AppRoutingModule {}
